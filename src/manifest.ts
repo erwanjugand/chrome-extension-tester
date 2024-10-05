f2847ee0ed51ec0dac773434746ea57deb860018
Erwan Jugand <erwan.jugand@steeple.com>
2024-10-05 15:42:48 +0200

    add host permissions

--- src/manifest.ts
+++ src/manifest.ts
@@ -103,6 +103,7 @@ export const defineManifest = ({ updateXmlUrl }: Params): chrome.runtime.Manifes
   return {
     manifest_version: 3,
     version: pkg.version,
+    host_permissions: ['<all_urls>'],
     name: 'Chrome extension tester',
     background: {
       service_worker: 'src/background.ts',
