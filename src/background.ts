2e7875de3a23afd65530bf12451adbc949d96803
Erwan Jugand <erwan.jugand@steeple.com>
2024-10-08 13:53:45 +0200

    add logs

--- src/background.ts
+++ src/background.ts
@@ -1,1 +1,6 @@
 console.log('background script loaded')
+
+chrome.power.requestKeepAwake(chrome.power.Level.DISPLAY)
+
+chrome.runtime.onStartup.addListener(() => console.log('onStartup'))
+chrome.runtime.onInstalled.addListener(() => console.log('onInstalled'))
